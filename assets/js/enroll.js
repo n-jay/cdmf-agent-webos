6714026af1f2ceb04069e1d6339b66c00a4a217a
Nuwan <w1608508@my.westminster.ac.uk>
2018-05-16 12:20:18 +0530

    Re-structure code to include refresh token retrieval

--- assets/js/enroll.js
+++ assets/js/enroll.js
@@ -3,20 +3,8 @@ $(document).ready(function() {
     var serverEndpoint = $('#endpoint').val();
     var deviceId;
 
-    function networkState() {
-        var request = webOS.service.request("luna://com.palm.connectionmanager", {
-            method: "getStatus",
-            onSuccess: function (inResponse) {
-                console.log("Result: " + JSON.stringify(inResponse));
-            },
-            onFailure: function (inError) {
-                console.log("Failed to get network state");
-                console.log("[" + inError.errorCode + "]: " + inError.errorText);
-                // To-Do something
-                return;
-            }
-        });
-    };
+    var urlEnroll = "http://10.100.4.109:8280/api/device-mgt/v1.0/device/agent/1.0.0/enroll";
+    var urlToken = "http://10.100.4.109:8280/token";
 
 
     var request = webOS.service.request("luna://com.palm.systemservice", {
@@ -49,31 +37,51 @@ $(document).ready(function() {
 
     function enroll() {
         $("#next").click(function() {
-            ajaxSend();
+            refreshAccessToken();
         });
 
         var data = {
             "name": "webOS TV",
             "type": "webOS",
-            "description": "descritption",
+            "description": "description",
             "deviceIdentifier": deviceId,
             "enrolmentInfo": {"ownership": "BYOD", "status": "ACTIVE"},
             "properties": [{"name": "propertyName", "value": "propertyValue"}]
         };
 
-        var url1 = "http://10.100.4.109:8280/api/device-mgt/v1.0/device/agent/1.0.0/enroll";
-
         function ajaxSend() {
             $.ajax({
                 type: "POST",
-                url: url1,
+                url: urlEnroll,
                 data: JSON.stringify(data),
                 headers: {
-                    'Authorization': 'Bearer a2fb9554-82d5-3c96-965f-3725724b7c38',
+                    'Authorization': 'Bearer 21bbc9e3-c12a-3ec6-ab59-6fa579649329',
                     'Content-Type': 'application/json'
                 },
                 success: function () {
                     console.log("success");
+                },
+            });
+        };
+    };
+
+    function refreshAccessToken() {
+
+        var refreshTokenBody = "grant_type=refresh_token&refresh_token=14688303-15ac-32d6-9898-1879a835995d&scope=PRODUCTION";
+
+        ajaxSend();
+
+        function ajaxSend() {
+            $.ajax({
+                type: "POST",
+                url: urlToken,
+                data: refreshTokenBody,
+                headers: {
+                    'Authorization': 'Basic RmxNVzMyZ2VubzlncE1velNYd1IyaDVmTzhnYTpoT0RmVDNWQTV3R0pDamRmNzAyMlEwcmZ2Zmth',
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                success: function (resp) {
+                    console.log(resp);
                 },
             });
         };
